fa57e53ab1be7c5b3c4c6ca909da44a139632c37
PoND SK <element.next00@gmail.com>
2021-02-09 15:29:50 +0700

    commit

--- assets/js/ChartJs.js
+++ assets/js/ChartJs.js
@@ -1,19 +1,160 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+let ChartUpdate = document.getElementById('ChartUpdate');
+let confirmed = [];
+let hospitalized = [];
+let recovered = [];
+let deaths = [];
+let strDate = [];
+let dateSplit = [];
+var thmonth = new Array("", "ม.ค.", "ก.พ.", "มี.ค.",
+    "เม.ย.", "พ.ค.", "มิ.ย.", "ก.ค.", "ส.ค.", "ก.ย.",
+    "ต.ค.", "พ.ย.", "ธ.ค.");
 
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: '#039245',
-            borderColor: '#039245',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+fetch("https://covid19.th-stat.com/api/open/timeline")
+    .then(response => response.json().then(data => {   
+        for (var i = 0; i < data.Data.length; i++) {
+            confirmed.push(data.Data[i].Confirmed);
+            hospitalized.push(data.Data[i].Hospitalized);
+            recovered.push(data.Data[i].Recovered);
+            deaths.push(data.Data[i].Deaths);
 
-    // Configuration options go here
-    options: {}
-});
+            var x = data.Data[i].Date;
+            var dd = x.split("/");
+            dateSplit.push(Number(dd[0]));
+            strDate.push(dd[1] + " " + thmonth[dateSplit[i]]);
+        }
+
+        ChartUpdate.innerHTML = data.UpdateDate;
+
+        var ctx = document.getElementById('Chart_TH').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+                labels: strDate,
+                datasets: [{
+                    label: 'ผู้ติดเชื้อ',
+                    pointRadius: 0,
+                    fill: false,
+                    lineTension: 0,
+                    borderWidth: 2,
+                    backgroundColor: 'rgb(255, 99, 132.5)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: confirmed,
+                },
+                {
+                    label: 'กำลังรักษา',
+                    pointRadius: 0,
+                    fill: false,
+                    lineTension: 0,
+                    borderWidth: 2,
+                    backgroundColor: '#f2c94c',
+                    borderColor: '#f2c94c',
+                    data: hospitalized,
+                },
+                {
+                    label: 'หายแล้ว',
+                    pointRadius: 0,
+                    fill: false,
+                    lineTension: 0,
+                    borderWidth: 2,
+                    backgroundColor: '#039245',
+                    borderColor: '#039245',
+                    data: recovered,
+                },
+                {
+                    label: 'เสียชีวิต',
+                    pointRadius: 0,
+                    fill: false,
+                    lineTension: 0,
+                    borderWidth: 2,
+                    backgroundColor: '#d22d36',
+                    borderColor: '#d22d36',
+                    data: deaths,
+                }
+
+                ]
+            },
+            // Configuration options go here
+            options: {
+                animation: {
+                    dutation: 0
+                },
+                title: {
+                    display: true,
+                    text: 'ข้อมูลผู้ติดเชื้อ Covid-19 ในประเทศไทย'
+                },
+                tooltips: {
+                    mode: 'index',
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+    }))
+    .catch(err => {
+        console.log(err);
+
+    })
+
+
+fetch("https://covid19.th-stat.com/api/open/timeline")
+    .then(response => response.json().then(data => {
+        let confirmed = [];
+        let hospitalized = [];
+        let recovered = [];
+        let deaths = [];
+        let arrayDate = [];
+        for (var i = 0; i < data.Data.length; i++) {
+            arrayDate.push(data.Data[i].Date);
+            confirmed.push(data.Data[i].Confirmed);
+            hospitalized.push(data.Data[i].Hospitalized);
+            recovered.push(data.Data[i].Recovered);
+            deaths.push(data.Data[i].Deaths);
+        }
+
+        var ctx = document.getElementById('Chart_World').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+                labels: arrayDate,
+                datasets: [{
+                    label: 'ผู้ติดเชื้อ',
+                    pointRadius: 0,
+                    fill: false,
+                    lineTension: 0,
+                    borderWidth: 2,
+                    backgroundColor: 'rgb(255, 99, 132.5)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: confirmed,
+                },
+                ]
+            },
+            // Configuration options go here
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+    }))
+    .catch(err => {
+        console.log(err);
+
+    })
+
--- assets/js/data_covid.js
+++ assets/js/data_covid.js
@@ -7,7 +7,7 @@ let Confirmed = document.getElementById('Confirmed');
 let Hospitalized = document.getElementById('Hospitalized');
 let Recovered = document.getElementById('Recovered');
 let Deaths = document.getElementById('Deaths');
-let Update= document.getElementById('Update');
+let Update = document.getElementById('Update');
 
 let sConfirmed = document.getElementById('sConfirmed');
 let sHospitalized = document.getElementById('sHospitalized');
@@ -15,31 +15,30 @@ let sRecovered = document.getElementById('sRecovered');
 let sDeaths = document.getElementById('sDeaths');
 
 fetch("https://covid19.th-stat.com/api/open/today")
-.then(response => response.json().then(data => {
-    console.log(data.NewConfirmed);
-    Confirmed.innerHTML = comma(data.Confirmed);
-    Hospitalized.innerHTML = comma(data.Hospitalized);
-    Recovered.innerHTML = comma(data.Recovered);
-    Deaths.innerHTML = comma(data.Deaths);
-    Update.innerHTML = data.UpdateDate;
+    .then(response => response.json().then(data => {
+        Confirmed.innerHTML = comma(data.Confirmed);
+        Hospitalized.innerHTML = comma(data.Hospitalized);
+        Recovered.innerHTML = comma(data.Recovered);
+        Deaths.innerHTML = comma(data.Deaths);
+        Update.innerHTML = data.UpdateDate;
 
-    NewConfirmed.innerHTML = Math.abs(comma(data.NewConfirmed));
-    NewHospitalized.innerHTML = Math.abs(comma(data.NewHospitalized));
-    NewRecovered.innerHTML = Math.abs(comma(data.NewRecovered));
-    NewDeaths.innerHTML = Math.abs(comma(data.NewDeaths));
+        NewConfirmed.innerHTML = Math.abs(comma(data.NewConfirmed));
+        NewHospitalized.innerHTML = Math.abs(comma(data.NewHospitalized));
+        NewRecovered.innerHTML = Math.abs(comma(data.NewRecovered));
+        NewDeaths.innerHTML = Math.abs(comma(data.NewDeaths));
 
-    status(data.NewConfirmed, sConfirmed)
-    status(data.NewHospitalized, sHospitalized)
-    status(data.NewRecovered, sRecovered)
-    status(data.NewDeaths, sDeaths)
+        status(data.NewConfirmed, sConfirmed)
+        status(data.NewHospitalized, sHospitalized)
+        status(data.NewRecovered, sRecovered)
+        status(data.NewDeaths, sDeaths)
 
-}))
-.catch(err=>{
-    console.log(err);
-    
-})
+    }))
+    .catch(err => {
+        console.log(err);
 
-function status(value, doc){
+    })
+
+function status(value, doc) {
     let up = "<i class='fas fa-angle-double-up'></i> เพิ่มขึ้น";
     let down = "<i class='fas fa-angle-double-down'></i> ลดลง";
     let equal = "<i class='fas fa-equals'></i> คงที่";
